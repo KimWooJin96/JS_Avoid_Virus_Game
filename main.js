e85559baaee47308bf5f4f228183709396179a3f
Woojin <sam4woojin@naver.com>
2021-02-24 23:45:20 +0900

    JS_ModifyItemPosition

--- main.js
+++ main.js
@@ -1,17 +1,31 @@
+"use strict";
+
 const field = document.querySelector(".game__field");
+const fieldRect = field.getBoundingClientRect();
+
 const carrot = "/img/carrot.png";
 const bug = "/img/bug.png";
 
-let index = 0;
+const CARROT_SIZE = 80;
+const BUG_SIZE = 50;
 
 function randomX(img) {
+  let rightOrLeft = 1;
   let X = 0;
   if (img === carrot) {
-    X = Math.floor(Math.random() * (400 - 80 + 1));
-    X *= Math.floor(Math.random() * 2) ? 1 : -1;
+    rightOrLeft = Math.round(Math.random()) ? 1 : -1;
+    if (rightOrLeft === -1) {
+      X = Math.floor(Math.random() * (fieldRect.width / 2 + 1)) * -1;
+    } else {
+      X = Math.floor(Math.random() * (fieldRect.width / 2 - CARROT_SIZE + 1));
+    }
   } else {
-    X = Math.floor(Math.random() * (400 - 50 + 1));
-    X *= Math.floor(Math.random() * 2) ? 1 : -1;
+    rightOrLeft = Math.round(Math.random()) ? 1 : -1;
+    if (rightOrLeft === -1) {
+      X = Math.floor(Math.random() * (fieldRect.width / 2 + 1)) * -1;
+    } else {
+      X = Math.floor(Math.random() * (fieldRect.width / 2 - BUG_SIZE + 1));
+    }
   }
   return X;
 }
@@ -19,42 +33,38 @@ function randomX(img) {
 function randomY(img) {
   let Y = 0;
   if (img === carrot) {
-    Y = Math.floor(Math.random() * (235 - 80 + 1));
+    Y = Math.floor(Math.random() * (fieldRect.height - CARROT_SIZE + 1));
   } else {
-    Y = Math.floor(Math.random() * (235 - 50 + 1));
+    Y = Math.floor(Math.random() * (fieldRect.height - BUG_SIZE + 1));
   }
   return Y;
 }
 
-function createImg(img) {
-  const newImg = document.createElement("img");
-  newImg.setAttribute("src", `${img}`);
-  newImg.id = index;
-  field.appendChild(newImg);
+function createImg(img, count) {
+  let index = 0;
 
-  const imgPositionX = randomX(img);
-  const imgPositionY = randomY(img);
-  console.log(imgPositionX, imgPositionY);
+  for (let i = 0; i < count; i++) {
+    console.log(i);
+    const newImg = document.createElement("img");
+    newImg.setAttribute("src", `${img}`);
+    newImg.id = index;
+    field.appendChild(newImg);
 
-  newImg.style.transform = `translate(${imgPositionX}px, ${imgPositionY}px)`;
-  // newImg.style.top = `${imgPositionX}px`;
-  // newImg.style.left = `${imgPositionY}px`;
+    const imgPositionX = randomX(img);
+    const imgPositionY = randomY(img);
+    // 가운데가 기준
+    newImg.style.transform = `translate(${imgPositionX}px, ${imgPositionY}px)`;
+    // 왼쪽이 기준
+    // newImg.style.top = `${0}px`;
+    // newImg.style.left = `${0}px`;
 
-  index += 1;
+    index += 1;
+  }
 }
 
-createImg(carrot);
-createImg(carrot);
-createImg(carrot);
-createImg(carrot);
-createImg(carrot);
-createImg(carrot);
-createImg(carrot);
-createImg(carrot);
-createImg(bug);
-createImg(bug);
-createImg(bug);
-createImg(bug);
-createImg(bug);
-createImg(bug);
-createImg(bug);
+function init() {
+  createImg(carrot, 10);
+  createImg(bug, 10);
+}
+
+init();
